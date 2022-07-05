648dfeb7113e23fc5fa7bebd9ca46901eab4c0f6
Tim Schofield <you@example.com>
2022-07-05 18:44:28 +0100

    getting there with coingecko conversion

--- Crypto.API/Models/CoinList.cs
+++ Crypto.API/Models/CoinList.cs
@@ -37,28 +37,54 @@ namespace Crypto.API.Models
         {
                 List<coins> coins = new List<coins>();
 
-                var response = makeAPICall();
+                //var responseC = makeAPICallCoinMarketCap();
+             
 
-                System.Console.WriteLine(response);
+                var responseG = makeAPICallCoinGecko();
 
-                var CoinMCap = JsonConvert.DeserializeObject<RootObject>(response);
+                System.Console.WriteLine(responseG);
+
+                //var CoinMCap = JsonConvert.DeserializeObject<RootObject>(responseC);
+               var CoinGecko = JsonConvert.DeserializeObject<List<coinGeckojson>>(responseG);
+               
 
                 listofCoins.Clear();
 
-                foreach (var item in CoinMCap.data)
+                // foreach (var item in CoinMCap.data)
+                // {
+                //     coins coin = new coins();
+                //     coin.Name = item.slug.ToString();    
+                //     if (!item.quote.USD.price.Equals(null)) { coin.Price = (decimal)item.quote.USD.price; } else { coin.Price = 0; }
+                //     if (!item.quote.USD.volume_24h.Equals(null)) { coin.Volume = (decimal)item.quote.USD.volume_24h; } else { coin.Volume = 0; }
+                //     if (!item.circulating_supply.Equals(null)) { coin.circulating = (decimal)item.circulating_supply; } else { coin.circulating = 0; }
+                //     if (!item.quote.USD.market_cap.Equals(null)) { coin.Marketcap = (decimal)item.quote.USD.market_cap; } else { coin.Marketcap = 0; }
+                //     if (!item.total_supply.Equals(null)) { coin.TotalSupply = (decimal)item.total_supply; } else { coin.TotalSupply = 0; }
+                //     if (!item.quote.USD.percent_change_1h.Equals(null)) { coin.PercentChange1hr = (decimal)item.quote.USD.percent_change_1h; } else { coin.PercentChange1hr = 0; }
+                //     if (!item.quote.USD.percent_change_24h.Equals(null)) { coin.PercentChange24hr = (decimal)item.quote.USD.percent_change_24h; } else { coin.PercentChange24hr = 0; }
+                //     if (!item.quote.USD.percent_change_7d.Equals(null)) { coin.PercentChange7day = (decimal)item.quote.USD.percent_change_7d; } else { coin.PercentChange7day = 0; }
+                //     coin.CoinMcapRank = item.cmc_rank;
+                //     coin.CoinID = item.id;
+
+                //     coins.Add(coin);
+                // }
+
+                foreach (var item in CoinGecko)
                 {
                     coins coin = new coins();
-                    coin.Name = item.slug.ToString();    
-                    if (!item.quote.USD.price.Equals(null)) { coin.Price = (decimal)item.quote.USD.price; } else { coin.Price = 0; }
-                    if (!item.quote.USD.volume_24h.Equals(null)) { coin.Volume = (decimal)item.quote.USD.volume_24h; } else { coin.Volume = 0; }
-                    if (!item.circulating_supply.Equals(null)) { coin.circulating = (decimal)item.circulating_supply; } else { coin.circulating = 0; }
-                    if (!item.quote.USD.market_cap.Equals(null)) { coin.Marketcap = (decimal)item.quote.USD.market_cap; } else { coin.Marketcap = 0; }
-                    if (!item.total_supply.Equals(null)) { coin.TotalSupply = (decimal)item.total_supply; } else { coin.TotalSupply = 0; }
-                    if (!item.quote.USD.percent_change_1h.Equals(null)) { coin.PercentChange1hr = (decimal)item.quote.USD.percent_change_1h; } else { coin.PercentChange1hr = 0; }
-                    if (!item.quote.USD.percent_change_24h.Equals(null)) { coin.PercentChange24hr = (decimal)item.quote.USD.percent_change_24h; } else { coin.PercentChange24hr = 0; }
-                    if (!item.quote.USD.percent_change_7d.Equals(null)) { coin.PercentChange7day = (decimal)item.quote.USD.percent_change_7d; } else { coin.PercentChange7day = 0; }
-                    coin.CoinMcapRank = item.cmc_rank;
-                    coin.CoinID = item.id;
+                    coin.Name = item.name.ToString();
+
+coin.Price = item.current_price;
+coin.Volume = item.total_volume;
+coin.circulating = item.circulating_supply;
+coin.Marketcap = item.market_cap_change_24h;
+coin.TotalSupply = 0;
+coin.PercentChange1hr = 0;
+coin.PercentChange24hr = 0;
+coin.PercentChange7day = 0;
+coin.CoinMcapRank = 0;
+coin.CoinID = 0;
+
+
 
                     coins.Add(coin);
                 }
@@ -145,6 +171,207 @@ namespace Crypto.API.Models
                     }
                 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                 foreach (var coin in coinlist)
                 {
                     PriceHistory ph = new PriceHistory();
@@ -159,11 +386,21 @@ namespace Crypto.API.Models
                 }
         }
 
-        static string makeAPICall()
+
+        static string makeAPICallCoinGecko()
+        {
+          var URL = new UriBuilder("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d");
+          var client = new WebClient();
+          return client.DownloadString(URL.ToString());
+        }
+
+
+        static string makeAPICallCoinMarketCap()
         {
 
             var URL = new UriBuilder("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest");
             // var URL = new UriBuilder("https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest");
+            //var URL = new UriBuilder("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false");
 
             var queryString = HttpUtility.ParseQueryString(string.Empty);
             queryString["start"] = "1";
@@ -175,7 +412,7 @@ namespace Crypto.API.Models
             var client = new WebClient();
 
             // swap top (Live) with below (test)    ////////////////////////////////////////////////////////////////////////////////////////////////  swap this one
-             client.Headers.Add("X-CMC_PRO_API_KEY", API_KEY_COINMCAP.API_KEY);
+            client.Headers.Add("X-CMC_PRO_API_KEY", API_KEY_COINMCAP.API_KEY);
             // client.Headers.Add("X-CMC_PRO_API_KEY", API_KEY_COINMCAP.API_KEY_TEST);
             // client.Headers.Add("X-CMC_PRO_API_KEY", API_KEY_COINMCAP.SANDBOX_API_KEY);
 
@@ -183,6 +420,9 @@ namespace Crypto.API.Models
             client.Headers.Add("Accepts", "application/json");
             return client.DownloadString(URL.ToString());
         }
+
+
+
 
         public IEnumerator GetEnumerator()
         {
@@ -193,95 +433,5 @@ namespace Crypto.API.Models
         }
     }
 
-
-
-
-
-
-
-    public class coins
-    {
-        public coins()
-        {
-        }
-
-        public coins(string name, decimal price)
-        {
-             this.Name = name;
-             this.Price = price;
-        }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public decimal Volume { get; set; }
-        public decimal circulating { get; set; }
-        public decimal Marketcap { get; set; }
-        public decimal TotalSupply { get; set; }
-        public decimal PercentChange1hr { get; set; }
-        public decimal PercentChange24hr { get; set; }
-        public decimal PercentChange7day { get; set; }
-        public int CoinMcapRank { get; set; }
-        public int CoinID { get; set; }
-        public DateTime LastUpdated { get; set; }
-
-    }
-
-    public class Status
-    {
-        public DateTime timestamp { get; set; }
-        public int error_code { get; set; }
-        public object error_message { get; set; }
-        public int elapsed { get; set; }
-        public int credit_count { get; set; }
-        public object notice { get; set; }
-    }
-
-    public class Platform
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-        public string symbol { get; set; }
-        public string slug { get; set; }
-        public string token_address { get; set; }
-    }
-
-    public class USD
-    {
-        public double? price { get; set; }
-        public double? volume_24h { get; set; }
-        public double? percent_change_1h { get; set; }
-        public double? percent_change_24h { get; set; }
-        public double? percent_change_7d { get; set; }
-        public double? market_cap { get; set; }
-        public DateTime last_updated { get; set; }
-    }
-
-    public class Quote
-    {
-        public USD USD { get; set; }
-    }
-
-    public class Datum
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-        public string symbol { get; set; }
-        public string slug { get; set; }
-        public int num_market_pairs { get; set; }
-        public DateTime date_added { get; set; }
-        public List<object> tags { get; set; }
-        public double? max_supply { get; set; }
-        public double circulating_supply { get; set; }
-        public double total_supply { get; set; }
-        public Platform platform { get; set; }
-        public int cmc_rank { get; set; }
-        public DateTime last_updated { get; set; }
-        public Quote quote { get; set; }
-    }
-
-    public class RootObject
-    {
-        public Status status { get; set; }
-        public List<Datum> data { get; set; }
-    }
 
 }
