0186ec985dbbecede946021b456f7ed6e1a07ee2
tim <test@email.com>
2023-07-07 10:58:29 +0100

    added new icons + working on server

--- Crypto.API/Data/DataContext.cs
+++ Crypto.API/Data/DataContext.cs
@@ -13,12 +13,12 @@ namespace Crypto.API.Data
 
         }
 
-        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
-        
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
 
         public DbSet<User> Users { get; set; }
         public DbSet<Portfolio> Portfolio { get; set; }
-        public DbSet<CoinsHodle> CoinsHodle {get; set;}
+        public DbSet<CoinsHodle> CoinsHodle { get; set; }
         public DbSet<Transactions> Transactions { get; set; }
         public DbSet<CoinNames> CoinNames { get; set; }
         public DbSet<PriceHistory> PriceHistory { get; set; }
@@ -34,18 +34,19 @@ namespace Crypto.API.Data
             // var password = API_KEY_COINMCAP.password;
             // var database = API_KEY_COINMCAP.database;
 
-            var server = API_KEY_COINMCAP.serverNameDev; 
+            var server = API_KEY_COINMCAP.serverNameDev;
             var port = API_KEY_COINMCAP.portDev;
             var user = API_KEY_COINMCAP.userDev;
             var password = API_KEY_COINMCAP.passwordDev;
             var database = API_KEY_COINMCAP.databaseDev;
 
 
-            optionsBuilder.UseSqlServer($"Server={server},{port};Database={database};User ID={user};Password={password};MultipleActiveResultSets=true");
+            //optionsBuilder.UseSqlServer($"Server={server},{port};Database={database};User ID={user};Password={password};MultipleActiveResultSets=true");
+            optionsBuilder.UseSqlServer($"Server={server};Database={database};User ID={user};Password={password};MultipleActiveResultSets=true");
 
         }
 
-      
+
     }
 
 }
