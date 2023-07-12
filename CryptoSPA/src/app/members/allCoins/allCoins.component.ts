e14efb02db119023f06622326f3713cf0716d36f
tim <test@email.com>
2023-07-12 17:12:45 +0100

    updated ui side removed bootstrap from many screens and just used css

--- CryptoSPA/src/app/members/allCoins/allCoins.component.ts
+++ CryptoSPA/src/app/members/allCoins/allCoins.component.ts
@@ -7,11 +7,8 @@ import { AuthService } from 'src/app/_services/auth.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { interval } from 'rxjs';
 import { HelperServiceService } from "./../../_services/HelperService.service";
-//import { debug } from 'util';
-
 
 @Component({
-  // tslint:disable-next-line:component-selector
   selector: 'app-allCoins',
   templateUrl: './allCoins.component.html',
   styleUrls: ['./allCoins.component.css']
@@ -27,19 +24,17 @@ export class AllCoinsComponent implements OnInit, OnDestroy {
   subscription: any;
   coinhodleids: string = '';
   transactions: any;
-
   PieChart = [];
 
   constructor(private userService: UserService, private authService: AuthService,
     private alertify: AlertifyService, public helperService: HelperServiceService) { }
 
-  async ngOnInit() {
-    await this.delay(1200);
-    if (this.loggedIn) {
-       this.loadUser(this.authService.decodedToken.nameid);
-       await this.delay(1000);
-      this.helperService.loadPieChart(this.AllcoinsList);
-    }
+  ngOnInit() {
+    this.delay(1200).then(() => {
+      if (this.loggedIn()) {
+        this.loadUser(this.authService.decodedToken.nameid);
+      }
+    });
     this.pageRefresh();
   }
 
@@ -48,147 +43,136 @@ export class AllCoinsComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-   loadUser(id: number) {
+  loadUser(id: number) {
     this.userService.getUser(id).subscribe((user: User) => {
       this.user = user;
-      this.getCoinPrices();
-      this.getValues(this.user);
+      this.getCoinPrices().then(() => {
+        this.getValues(this.user).then(() => {
+          this.delay(1000).then(() => {
+            this.helperService.loadPieChart(this.AllcoinsList);
+          });
+        });
+      });
     }, error => {
       this.alertify.error(error);
     });
   }
 
   pageRefresh() {
-    this.subscription = interval(500 * 60).subscribe(async x => {
-      if (this.loggedIn) {
+    this.subscription = interval(500 * 60).subscribe(x => {
+      if (this.loggedIn()) {
         this.loadUser(this.authService.decodedToken.nameid);
-        await this.delay(2000);
-        this.helperService.loadPieChart(this.AllcoinsList);
       }
-     });
-   }
+    });
+  }
 
-   getCoinPrices() {
-    this.userService.getCoinPrices().subscribe(Response => {
-      this.coins = Response;
-      for (let i = 0; i < this.coins.length; i++) {
-        if (this.coins[i].coinID === 'bitcoin') {
-          this.bitcoinPrice = this.coins[i].price;
+  getCoinPrices() {
+    return new Promise<void>((resolve, reject) => {
+      this.userService.getCoinPrices().subscribe(Response => {
+        this.coins = Response;
+        for (let i = 0; i < this.coins.length; i++) {
+          if (this.coins[i].coinID === 'bitcoin') {
+            this.bitcoinPrice = this.coins[i].price;
+          }
         }
-      }
-    }, error => {
-      console.log(error);
+        resolve();
+      }, error => {
+        console.log(error);
+        reject(error);
+      });
     });
   }
 
   getValues(user: User) {
-
-    this.AllcoinsnImageList = [];
-    this.total = 0;
-    this.AllcoinsList = [];
-    this.coinFound = false;
-
-    this.userService.getAllPortfolioCoins(user.id).subscribe((portfolios: Portfolio[]) => {
-
-
-       this.coinhodleids = '';                                                                         //  gets all the coinhodle ids
-       for (const portfolio of portfolios) {                                                      //  in all the portfolios
-        if (portfolio && portfolio.coinsHodle.length > 0) {                                       //
-          for (const co of portfolio.coinsHodle) {                                                //
-             this.coinhodleids = this.coinhodleids + co.id + ',';                                  //
-            this.coinFound = false;
-
-
-
-            for (let i = 0; i < this.AllcoinsList.length; i++) {                                  // loop through each AllcoinsList
-              if (co.coinID === this.AllcoinsList[i].coinID) {                                    // if the name matches
-                this.coinFound = true;
-              }
-            }
-
-            if (this.coinFound === false) {
-              for (let i = 0; i < this.coins.length; i++) {
-                if (co.coinID === this.coins[i].coinID) {
-                  co.price = this.coins[i].price;
-                  co.name = this.coins[i].name;
+    return new Promise<void>((resolve, reject) => {
+      this.AllcoinsnImageList = [];
+      this.total = 0;
+      this.AllcoinsList = [];
+      this.coinFound = false;
+      this.userService.getAllPortfolioCoins(user.id).subscribe((portfolios: Portfolio[]) => {
+        this.coinhodleids = '';
+        for (const portfolio of portfolios) {
+          if (portfolio && portfolio.coinsHodle.length > 0) {
+            for (const co of portfolio.coinsHodle) {
+              this.coinhodleids = this.coinhodleids + co.id + ',';
+              this.coinFound = false;
+              for (let i = 0; i < this.AllcoinsList.length; i++) {
+                if (co.coinID === this.AllcoinsList[i].coinID) {
+                  this.coinFound = true;
                 }
               }
-              this.AllcoinsList.push(co);                                                         // add coin and qty to Allcoins list
-            }
-
-          }                                                                                       //
-        }                                                                                         //
-       }                                                                                          //
-
-
-       this.userService.getTransactions(this.coinhodleids).subscribe(async Response => {
-        this.transactions = Response;
- 
-        for (let i = 0; i < this.AllcoinsList.length; i++) {
- 
-           for (const transaction of this.transactions) {
-             if (transaction.coinsHodle.coinID === this.AllcoinsList[i].coinID) {
-               this.AllcoinsList[i].quantity = this.AllcoinsList[i].quantity + transaction.amountBuy;     //  sets quantity
-               this.AllcoinsList[i].quantity = this.AllcoinsList[i].quantity - transaction.amountSell;    //
-             }
-           }
- 
-        }
-
-
-                // copy portfolio coins into coinsnImageList
-                for (let i = 0; i < this.AllcoinsList.length; i++) {
-                  const coinHodle = this.AllcoinsList[i];
-                  this.AllcoinsnImageList.push(coinHodle);
-                }
-            
-                for (let i = 0; i < this.AllcoinsnImageList.length; i++) {                                                  //
-                  this.AllcoinsnImageList[i].imageLocation = 'assets/images/' + this.AllcoinsnImageList[i].coinID + '.png';   // images
-                }
-
-              for (const AllCoin of this.AllcoinsList) {                                //
-                  if (AllCoin.price > 0 && AllCoin.quantity > 0) {                      // Totals 00000
-                    this.total = this.total + (AllCoin.price * AllCoin.quantity);       //
+              if (this.coinFound === false) {
+                for (let i = 0; i < this.coins.length; i++) {
+                  if (co.coinID === this.coins[i].coinID) {
+                    co.price = this.coins[i].price;
+                    co.name = this.coins[i].name;
                   }
+                }
+                this.AllcoinsList.push(co);
               }
-
+            }
+          }
+        }
+        this.userService.getTransactions(this.coinhodleids).subscribe(async Response => {
+          this.transactions = Response;
+          for (let i = 0; i < this.AllcoinsList.length; i++) {
+            for (const transaction of this.transactions) {
+              if (transaction.coinsHodle.coinID === this.AllcoinsList[i].coinID) {
+                this.AllcoinsList[i].quantity = this.AllcoinsList[i].quantity + transaction.amountBuy;
+                this.AllcoinsList[i].quantity = this.AllcoinsList[i].quantity - transaction.amountSell;
+                // Limit the quantity to 4 decimal places
+               this.AllcoinsList[i].quantity = Number(this.AllcoinsList[i].quantity.toFixed(4));
+              }
+            }
+          }
+          for (let i = 0; i < this.AllcoinsList.length; i++) {
+            const coinHodle = this.AllcoinsList[i];
+            this.AllcoinsnImageList.push(coinHodle);
+          }
+          for (let i = 0; i < this.AllcoinsnImageList.length; i++) {
+            this.AllcoinsnImageList[i].imageLocation = 'assets/images/' + this.AllcoinsnImageList[i].coinID + '.png';
+          }
+          for (const AllCoin of this.AllcoinsList) {
+            if (AllCoin.price > 0 && AllCoin.quantity > 0) {
+              this.total = this.total + (AllCoin.price * AllCoin.quantity);
+            }
+          }
+          this.FormatNumbers();
+          resolve();
+        });
+      }, error => {
+        this.alertify.error(error);
+        reject(error);
       });
-
-         this.FormatNumbers();
-
-
-
-    }, error => {
-      this.alertify.error(error);
     });
   }
 
   FormatNumbers() {
-
+    const decimalPlace = window.innerWidth < 900 ? 4 : 8;
+  
     for (let i = 0; i < this.AllcoinsList.length; i++) {
-
+  
       ///
       /// Format prices to correct decimal places.
       ///
-
+  
       if (this.AllcoinsList[i].price < 1) {
-        this.AllcoinsList[i].price = Number(this.AllcoinsList[i].price.toFixed(8));
+        this.AllcoinsList[i].price = Number(this.AllcoinsList[i].price.toFixed(decimalPlace));
       }
-
+  
       if (this.AllcoinsList[i].price > 1 && this.AllcoinsList[i].price < 100) {
-        this.AllcoinsList[i].price = Number(this.AllcoinsList[i].price.toFixed(4));
+        this.AllcoinsList[i].price = Number(this.AllcoinsList[i].price.toFixed(decimalPlace));
       }
-
+  
       if (this.AllcoinsList[i].price > 100) {
         this.AllcoinsList[i].price = Number(this.AllcoinsList[i].price.toFixed(2));
       }
-
     }
-
   }
+  
 
   delay(ms: number) {
-    return new Promise( resolve => setTimeout(resolve, ms) );
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
   loggedIn() {
